c9a9415466d2292000b302c05081a203868e8793
Nabila Ayaba <nabila.ayaba@gmail.com>
2023-11-17 13:47:23 -0500

    Create game board with css & html

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-console.log('JS is linked!')
+// game displays rows and columns of circles
+// player is notified of who's turn it is
+// player clicks a button to make a move
+// the player's move is displayed on the board
+// the turn switches from p1 to p2
+// p2 can select a move with the same process
+// at the end of the game, announce the winner
+// allow the opportunity to play again
+
+/*----- constants -----*/
+
+
+/*----- state variables -----*/
+
+
+/*----- cached elements  -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
